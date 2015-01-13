e06299aec78772cf75c750118bed5ab1513e4b4a
Nicolas Chevalier <niccheva@student.42.fr>
2015-01-13 08:02:22 +0100

    add setters and getters

--- classGenerator.rb
+++ classGenerator.rb
@@ -7,7 +7,7 @@
 #    By: niccheva <niccheva@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/01/10 23:06:54 by niccheva          #+#    #+#              #
-#    Updated: 2015/01/11 09:56:31 by niccheva         ###   ########.fr        #
+#    Updated: 2015/01/13 08:01:57 by niccheva         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -42,6 +42,32 @@ def operatorComment(f)
   end
   f.write " Operator Overload "
   28.times do
+    f.write "*"
+  end
+  f.write " */\n"
+end
+
+def getterComment(f)
+  f.write "/* "
+
+  32.times do
+    f.write "*"
+  end
+  f.write " Getters "
+  33.times do
+    f.write "*"
+  end
+  f.write " */\n"
+end
+
+def setterComment(f)
+  f.write "/* "
+
+  32.times do
+    f.write "*"
+  end
+  f.write " Setters "
+  33.times do
     f.write "*"
   end
   f.write " */\n"
@@ -108,15 +134,15 @@ if File.exist?(name + ".class.hpp")
   exit
 end
 
+privates = getPrivate ARGV
+protecteds = getProtected ARGV
+publics = getPublic ARGV
+
 File.open(name + ".class.hpp", 'w') do |f|
   f.write "\n#ifndef\t\t#{name.upcase}_CLASS_HPP\n"
   f.write "# define\t#{name.upcase}_CLASS_HPP\n\n"
 
   f.write "class #{name} {\n"
-
-  privates = getPrivate ARGV
-  protecteds = getProtected ARGV
-  publics = getPublic ARGV
 
   if privates
     f.write "private:\n"
@@ -149,6 +175,30 @@ File.open(name + ".class.hpp", 'w') do |f|
   operatorComment f
   f.write "\t#{name} const\t\t\t&operator=(#{name} const & rhs);\n\n"
 
+  getterComment f
+  if privates
+    privates.each do |hash|
+      f.write "\t#{hash[0]}\t\t\t#{name}::get#{hash[1].capitalize}(void) const;\n"
+    end
+  end
+  if protecteds
+    protecteds.each do |hash|
+      f.write "\t#{hash[0]}\t\t\t#{name}::get#{hash[1].capitalize}(void) const;\n"
+    end
+  end
+
+  setterComment f
+  if privates
+    privates.each do |hash|
+      f.write "\tvoid\t\t\t#{name}::set#{hash[1].capitalize}(#{hash[0]} #{hash[1]});\n"
+    end
+  end
+  if protecteds
+    protecteds.each do |hash|
+      f.write "\tvoid\t\t\t#{name}::set#{hash[1].capitalize}(#{hash[0]} #{hash[1]});\n"
+    end
+  end
+
   f.write "};\n\n"
   f.write "#endif //\t#{name.upcase}_CLASS_HPP\n"
 end
@@ -170,5 +220,29 @@ File.open(name + ".class.cpp", 'w') do |f|
 
   operatorComment f
   f.write "\n#{name} const\t\t\t&#{name}::operator=(#{name} const & rhs) {\n\n\treturn (*this);\n}\n"
+
+  getterComment f
+  if protecteds
+    protecteds.each do |hash|
+      f.write "#{hash[0]}\t\t\t#{name}::get#{hash[1].capitalize}(void) const {\n\treturn (this->_#{hash[1]});\n}\n"
+    end
+  end
+  if privates
+    privates.each do |hash|
+      f.write "#{hash[0]}\t\t\t#{name}::get#{hash[1].capitalize}(void) const {\n\treturn (this->_#{hash[1]});\n}\n"
+    end
+  end
+
+  setterComment f
+  if privates
+    privates.each do |hash|
+      f.write "void\t\t\t#{name}::set#{hash[1].capitalize}(#{hash[0]} #{hash[1]}) {\n\tthis->_#{hash[1]} = #{hash[1]}\n}\n"
+    end
+  end
+  if protecteds
+    protecteds.each do |hash|
+      f.write "void\t\t\t#{name}::set#{hash[1].capitalize}(#{hash[0]} #{hash[1]}) {\n\tthis->_#{hash[1]} = #{hash[1]}\n}\n"
+    end
+  end
 
 end
